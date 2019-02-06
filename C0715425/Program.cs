b217b37cba1146bdf3b06564e74f99a94663ce2c
visualstudio <visualstudio@122-PC09>
2019-02-06 10:53:33 -0500

    Add project files.

---
+++ C0715425/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C0715425
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
